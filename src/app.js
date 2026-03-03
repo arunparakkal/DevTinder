a9553e219b523486f780cc2a6d7cb044e68a3e51
arunparakkal <arunaru0034@gmail.com>
2026-03-04 00:12:48 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -1,22 +1,13 @@
 const express = require("express");
 const app = express();
+ 
+  app.get("/user/:userId/:name/:pasword", (req, res) => {
+    console.log(req.params);
+    res.send("get user succes");
+  })
+   
 
-  app.get("/user", (req, res) => {
-    res.send("get user succes")
-  })
-  app.post("/user", (req, res) => {
-    res.send({firstName:"Arun", LastName: "Sunil" })
-  })
-  app.put("/user", (req, res) => {
-    res.send("put is reqest is succesful get");
-  })
-  app.delete("/user", (req, res) => {
-     res.send("user is deleted");
-  }) 
-  console.log("hellow world");
-   app.use("/user",(req, res) => {
-    res.send("this is from app.use!!!!");
- })
+ 
 app.listen(3000, () => {
     console.log("server is running");
 });
