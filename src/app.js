ef1268512d3f35e97a924750b65acd5946d9d10f
arunparakkal <arunaru0034@gmail.com>
2026-03-03 18:37:43 +0530

    exploring routes

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const app = express();
-// app.use("/", (req, res, next) => {
-//     res.send("this is from da shboard");
-// })
-app.use("/test", (req, res, next) => {
-    res.send("this is from test");
-})
-app.use("/hello", (req, res, next) => {
-    res.send("hello  hello w helloo  "); 
 
-}) 
+  app.get("/user", (req, res) => {
+    res.send("get user succes")
+  })
+  app.post("/user", (req, res) => {
+    res.send({firstName:"Arun", LastName: "Sunil" })
+  })
+  app.put("/user", (req, res) => {
+    res.send("put is reqest is succesful get");
+  })
+  app.delete("/user", (req, res) => {
+     res.send("user is deleted");
+  })
+   app.use("/user",(req, res) => {
+    res.send("this is from app.use!!!!");
+ })
 app.listen(3000, () => {
     console.log("server is running");
 });
